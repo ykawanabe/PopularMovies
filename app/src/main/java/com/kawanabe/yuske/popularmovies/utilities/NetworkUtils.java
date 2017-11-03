fdd831abdae63fc5c9e3f9975e66b60eb532c561
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2017-11-03 08:30:33 -0700

    Add network util

---
+++ app/src/main/java/com/kawanabe/yuske/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,58 @@
+package com.kawanabe.yuske.popularmovies.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by yusuke on 11/3/17.
+ */
+
+public class NetworkUtils {
+
+    final static String BASE_URL = "http://api.themoviedb.org/3/movie";
+    final static String POPULAR_PATH = "popular";
+    final static String PARAM_API_KEY = "api_key";
+    final static String API_KEY = "";
+
+
+    public static URL buildUrl() {
+    Uri buildUri = Uri.parse(BASE_URL).buildUpon()
+            .appendPath(POPULAR_PATH)
+            .appendQueryParameter(PARAM_API_KEY, API_KEY)
+            .build();
+
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
