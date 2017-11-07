d44c708f24342aeb9d6f0a0b02e3e318d394715f
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2017-11-07 09:06:57 -0800

    Add list view settings

--- app/src/main/java/com/kawanabe/yuske/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/kawanabe/yuske/popularmovies/MovieAdapter.java
@@ -1,6 +1,8 @@
 package com.kawanabe.yuske.popularmovies;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,6 +19,14 @@ import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
 
+    Movie[] mMovies;
+    MovieAdapterOnClick mClickHandler;
+
+    public MovieAdapter(MovieAdapterOnClick clickHander) {
+        mClickHandler = clickHander;
+    }
+
+
     public interface MovieAdapterOnClick {
         void onClick(Movie movie);
     }
@@ -32,22 +42,34 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapter
 
         @Override
         public void onClick(View v) {
+            int index = getAdapterPosition();
+            Movie selected = mMovies[index];
+            mClickHandler.onClick(selected);
+        }
 
+        public void bind(Movie movie) {
+            mTextView.setText(movie.title);
         }
     }
 
     @Override
     public MovieAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflator = LayoutInflater.from(context);
+        int id = R.layout.movie_list_item;
+
+        View view = inflator.inflate(id, parent, false);
+        return new MovieAdapterViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MovieAdapterViewHolder holder, int position) {
-
+        Movie movie = mMovies[position];
+        holder.bind(movie);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mMovies.length;
     }
 }
